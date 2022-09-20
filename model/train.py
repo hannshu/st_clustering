29e9d71e4a750b810576fe4b6a00538c7c41b727
shuhan <contact@shuhan123.com>
2022-09-20 17:11:43 +0800

    更新model

--- model/train.py
+++ model/train.py
@@ -1,45 +1,136 @@
 import scanpy as sc
+import os
 import torch
 from STAGATE import STAGATE
 from utilities import *
 
-def train(adata, radius=300, components_spatial=3000, 
-          components_features=200, embedding_dim=128, lr=1e-3, epochs=100):
+from sklearn import metrics
+from sklearn.preprocessing import LabelEncoder 
+from sklearn.cluster import KMeans
 
-    sc.pp.highly_variable_genes(adata, flavor="seurat_v3", n_top_genes=3000)
-    sc.pp.normalize_total(adata, target_sum=1e4)
-    sc.pp.log1p(adata)
+def train(adata, radius=None, knears=None, components_spatial=3000, 
+          walk_length=45, walk_times=10,
+          components_features=256, embedding_dim=60, lr=1e-3,
+          epochs_spatial=500, epochs_features=500, seed=2022,
+          hidden_layer_dim_spatial=512, hidden_layer_dim_features=128,
+          device = 'cuda' if torch.cuda.is_available() else 'cpu'):
+    
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
 
-    data_spatial = build_spatial_graph(adata, radius, components_spatial)
-    data_features = build_feature_graph(adata, components_features, data_spatial)
+    data_spatial = build_spatial_graph(adata, components_spatial, radius, knears)
+    data_features = build_feature_graph(adata, components_features, 
+                                        data_spatial, walk_length, walk_times, seed)
 
     # 建立模型
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
     model_spatial = STAGATE(hidden_dims=[components_spatial, 
-                            (components_spatial + embedding_dim // 2) // 2,
+                            hidden_layer_dim_spatial,
                             embedding_dim // 2]).to(device)
     model_features = STAGATE(hidden_dims=[components_features, 
-                            (components_features + embedding_dim // 2) // 2,
+                            hidden_layer_dim_features,
                             embedding_dim // 2]).to(device)
     loss_spatial_func = torch.nn.MSELoss().to(device)
     loss_features_func = torch.nn.MSELoss().to(device)
-    optim_spatial = torch.optim.Adam(model_spatial.parameters(), lr=lr, weight_decay=0.005)
-    optim_features = torch.optim.Adam(model_features.parameters(), lr=lr, weight_decay=0.005)
+    optim_spatial = torch.optim.Adam(model_spatial.parameters(), lr=lr, weight_decay=1e-4)
+    optim_features = torch.optim.Adam(model_features.parameters(), lr=lr, weight_decay=1e-4)
+
+    data_spatial = data_spatial.to(device)
+    data_features = data_features.to(device)
+    print('>>> spatial model data(features) size: ({}, {})'.format(data_spatial.x.shape[0], data_spatial.x.shape[1]))
+    print('>>> feature model data(deepwalk) size: ({}, {})'.format(data_features.x.shape[0], data_features.x.shape[1]))
 
     model_spatial.train()
-    model_features.train()
-    for _ in tqdm(range(epochs), desc='training model'):
+    for _ in tqdm(range(epochs_spatial), desc='training spatial model'):
         optim_spatial.zero_grad()
-        optim_features.zero_grad()
         _, output_spatial = model_spatial(data_spatial)
-        _, output_features = model_features(data_features)
-        loss_spatial = loss_spatial_func(output_spatial, data_spatial.x)
-        loss_features = loss_features_func(output_features, data_features.x)
+        loss_spatial = loss_spatial_func(data_spatial.x, output_spatial)
         loss_spatial.backward()
-        loss_features.backward()
+        torch.nn.utils.clip_grad_norm_(model_spatial.parameters(), 5)
         optim_spatial.step()
+        
+    model_features.train()
+    for _ in tqdm(range(epochs_features), desc='training features model'):
+        optim_features.zero_grad()
+        _, output_features = model_features(data_features)
+        loss_features = loss_features_func(data_features.x, output_features)
+        loss_features.backward()
+        torch.nn.utils.clip_grad_norm_(model_features.parameters(), 5)
         optim_features.step()
 
-    embedding_spatial, _ = model_spatial(data_spatial)
-    embedding_features, _ = model_features(data_features)
-    return torch.cat([embedding_spatial, embedding_features], dim=1).cpu().detach().numpy()
+    model_spatial.eval()
+    model_features.eval()
+    with torch.no_grad():
+        embedding_spatial, _ = model_spatial(data_spatial.to(device))
+        embedding_features, _ = model_features(data_features.to(device))
+    return (torch.cat([embedding_spatial, embedding_features], dim=1).cpu().detach().numpy(), 
+            embedding_spatial.cpu().detach().numpy(), 
+            embedding_features.cpu().detach().numpy())
+    
+def evaluate(adata, embed, spatial, features, pca, n_clusters, seed=2022):
+    adata = mclust_R(adata, n_clusters, embed, random_seed=seed, name='mclust_embed')
+    adata = mclust_R(adata, n_clusters, embed, random_seed=seed, name='mclust_spatial')
+    adata.obs['embed'] = [str(x) for x in KMeans(n_clusters=n_clusters, random_state=seed).fit(embed).predict(embed)]
+    adata.obs['pred'] = [str(x) for x in KMeans(n_clusters=n_clusters, random_state=seed).fit(pca).predict(pca)]
+    adata.obs['pred_spatial'] = [str(x) for x in KMeans(n_clusters=n_clusters, random_state=seed).fit(spatial).predict(spatial)]
+    adata.obs['pred_features'] = [str(x) for x in KMeans(n_clusters=n_clusters, random_state=seed).fit(features).predict(features)]
+    
+    label = LabelEncoder().fit_transform(adata.obs['cluster'])
+    pred_mclust_embed = LabelEncoder().fit_transform(adata.obs['mclust_embed'])
+    pred_mclust_spatial = LabelEncoder().fit_transform(adata.obs['mclust_spatial'])
+    pred_embed = LabelEncoder().fit_transform(adata.obs['embed'])
+    pred = LabelEncoder().fit_transform(adata.obs['pred'])
+    pred_spatial = LabelEncoder().fit_transform(adata.obs['pred_spatial'])
+    pred_features = LabelEncoder().fit_transform(adata.obs['pred_features'])
+    
+    print('pred_mclust_embed:', metrics.adjusted_rand_score(label, pred_mclust_embed))
+    print('pred_mclust_spatial:', metrics.adjusted_rand_score(label, pred_mclust_spatial))
+    print('pred_embed:', metrics.adjusted_rand_score(label, pred_embed))
+    print('pred_pca:', metrics.adjusted_rand_score(label, pred))
+    print('pred_spatial:', metrics.adjusted_rand_score(label, pred_spatial))
+    print('pred_features:', metrics.adjusted_rand_score(label, pred_features))
+    
+    sc.pl.spatial(adata, color=['cluster', 'mclust_embed', 'mclust_spatial', 'embed', 'pred', 'pred_spatial', 'pred_features'])
+    
+def get_dlpfc_data(id):
+    section_list = ['151507', '151508', '151509', '151510', '151669', '151670', 
+                    '151671', '151672', '151673', '151674', '151675', '151676']
+    section_id = section_list[id]
+
+    adata = sc.read_visium(path=os.path.join('..', 'dataset', 'DLPFC', section_id))
+    adata.var_names_make_unique()
+
+    Ann_df = pd.read_csv(os.path.join('..', 'dataset', 'DLPFC', section_id, 
+                                      'ground_truth.txt'), sep='\t', header=None, index_col=0)
+    Ann_df.columns = ['Ground Truth']
+    adata.obs['cluster'] = Ann_df.loc[adata.obs_names, 'Ground Truth']
+    
+    sc.pp.highly_variable_genes(adata, flavor="seurat_v3", n_top_genes=3000)
+    sc.pp.normalize_total(adata, target_sum=1e4)
+    sc.pp.log1p(adata)
+    
+    return adata, len(set(adata.obs['cluster'])) - 1
+
+def mclust_R(adata, num_cluster, data, name, modelNames='EEE', random_seed=2020):
+    """\
+    Clustering using the mclust algorithm.
+    The parameters are the same as those in the R package mclust.
+    """
+    
+    np.random.seed(random_seed)
+    import rpy2.robjects as robjects
+    robjects.r.library("mclust")
+
+    import rpy2.robjects.numpy2ri
+    rpy2.robjects.numpy2ri.activate()
+    r_random_seed = robjects.r['set.seed']
+    r_random_seed(random_seed)
+    rmclust = robjects.r['Mclust']
+
+    res = rmclust(rpy2.robjects.numpy2ri.numpy2rpy(data), num_cluster, modelNames)
+    mclust_res = np.array(res[-2])
+
+    adata.obs[name] = mclust_res
+    adata.obs[name] = adata.obs[name].astype('int')
+    adata.obs[name] = adata.obs[name].astype('category')
+    return adata
--- model/utilities.py
+++ model/utilities.py
@@ -8,15 +8,27 @@ from torch_geometric.data import Data
 from torch_geometric.nn import Node2Vec
 from tqdm import tqdm
 
+from gensim.models import word2vec
+import networkx as nx
+import random
+import os
 
-def build_spatial_graph(adata, radius, components):
+
+def build_spatial_graph(adata, components, radius=None, knears=None):
+    
+    assert (None == radius and knears) or (radius and None == knears)
+    
     adata = adata[:, adata.var['highly_variable']]
     coor = pd.DataFrame(adata.obsm['spatial'])  
     coor.index = adata.obs.index
     coor.columns = ['imagerow', 'imagecol']
 
-    nbrs = NearestNeighbors(radius=radius).fit(coor)
-    _, indices = nbrs.radius_neighbors(coor, return_distance=True)
+    if (radius):
+        nbrs = NearestNeighbors(radius=radius).fit(coor)
+        _, indices = nbrs.radius_neighbors(coor, return_distance=True)
+    else:
+        nbrs = NearestNeighbors(n_neighbors=knears+1).fit(coor)
+        _, indices = nbrs.kneighbors(coor)
 
     edge_list = [[], []]
     for i in range(len(indices)):
@@ -24,38 +36,93 @@ def build_spatial_graph(adata, radius, components):
             edge_list[0].append(i)
             edge_list[1].append(indices[i][j])
 
-    pca = PCA(n_components=components).fit(adata.X.todense()).transform(adata.X.todense())
+    if (components != adata.X.todense().shape[1]):
+        pca = PCA(n_components=components).fit(adata.X.todense()).transform(adata.X.todense())
+    else:
+        pca = adata.X.todense()
+
+    print('>>> The graph contains {} edges.'.format(len(edge_list[0])))
+    
     data = Data(edge_index=torch.LongTensor(np.array([edge_list[0], edge_list[1]])), 
                 x=torch.FloatTensor(pca))
 
     print('>>> Building spatial graph success!')
     return data
 
+def random_walk(graph, walk_length, walk_times, seed):
+    random.seed(seed)
+    sentences = []
+    
+    for node in graph.nodes:
+        for _ in range(walk_times):
+            sentence = [node]
+            cur_node = node
+            for _ in range(walk_length - 1):
+                neighbors = list(graph.neighbors(cur_node))
+                if (0 == len(neighbors)):
+                    break
+                cur_node = neighbors[random.randint(0, len(neighbors) - 1)]
+                sentence.append(cur_node)
+            sentences.append(sentence)
+            
+    return sentences
 
-def build_feature_graph(adata, features, spatial_data):
+
+def build_feature_graph(adata, features, spatial_data, walk_length, walk_times, seed):
     adata = adata[:, adata.var['highly_variable']]
     sc.pp.neighbors(adata)
     edge_list = np.nonzero(adata.obsp['distances'].todense())
 
     # node2vec
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    model = Node2Vec(spatial_data.edge_index, embedding_dim=features, walk_length=20,
-                     context_size=10, walks_per_node=10, num_negative_samples=1).to(device)
-    loader = model.loader(batch_size=128, shuffle=True, num_workers=4)
-    optim = torch.optim.Adam(model.parameters(), lr=0.01)
+    # model = Node2Vec(spatial_data.edge_index, embedding_dim=features, walk_length=45,
+    #                  context_size=10, walks_per_node=10, num_negative_samples=5).to(device)
+    # loader = model.loader(batch_size=128, shuffle=True, num_workers=4)
+    # optim = torch.optim.Adam(model.parameters(), lr=0.01)
 
-    model.train()
-    for _ in tqdm(range(200), desc='training node embeddings'):
-        for pos_rw, neg_rw in loader:
-            optim.zero_grad()
-            loss = model.loss(pos_rw.to(device), neg_rw.to(device))
-            loss.backward()
-            optim.step()
+    # model.train()
+    # for _ in tqdm(range(epochs), desc='training node embeddings'):
+    #     for pos_rw, neg_rw in loader:
+    #         optim.zero_grad()
+    #         loss = model.loss(pos_rw.to(device), neg_rw.to(device))
+    #         loss.backward()
+    #         optim.step()
 
-    model.eval()
-    x = model()
+    # model.eval()
+    # x = model()
+    
+    graph = nx.Graph()
+    for i in range(len(spatial_data.edge_index[0])):
+        graph.add_edge(int(spatial_data.edge_index[0][i]), 
+                       int(spatial_data.edge_index[1][i]))
+    sentences = random_walk(graph, walk_length, walk_times, seed)
+    print('>>> Random walk finished!')
+    
+    word2vec_model = word2vec.Word2Vec(sentences, vector_size=features, sg=1, hs=0, 
+                                       negative=5, seed=seed, epochs=500, workers=12)
+    x = word2vec_model.wv.vectors
+    
     data = Data(edge_index=torch.LongTensor(np.array([edge_list[0], edge_list[1]])),
                 x=torch.FloatTensor(x))
-
+    
     print('>>> Building features graph success!')
     return data
+
+if __name__ == '__main__':
+    section_list = ['151507', '151508', '151509', '151510', '151669', '151670', '151671', '151672', '151673', '151674', '151675', '151676']
+    section_id = section_list[0]
+
+    # 读取section_id这个slice的数据
+    adata = sc.read_visium(path=os.path.join('dataset', 'DLPFC', section_id))
+    adata.var_names_make_unique()
+    
+    sc.pp.highly_variable_genes(adata, flavor="seurat_v3", n_top_genes=3000)
+    sc.pp.normalize_total(adata, target_sum=1e4)
+    sc.pp.log1p(adata)
+
+    # 为数据添加ground truth
+    Ann_df = pd.read_csv(os.path.join('dataset', 'DLPFC', section_id, 'ground_truth.txt'), sep='\t', header=None, index_col=0)
+    Ann_df.columns = ['Ground Truth']
+    adata.obs['Cluster'] = Ann_df.loc[adata.obs_names, 'Ground Truth']
+    
+    data_spatial = build_spatial_graph(adata, 300, 3000)    
+    data_features = build_feature_graph(adata, 500, data_spatial, 45, 10, 2022)
